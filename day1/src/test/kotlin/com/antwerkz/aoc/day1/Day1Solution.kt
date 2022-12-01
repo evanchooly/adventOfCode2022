acfd52c64adfc2b1a03a38107d250539019162f4
Justin Lee <evanchooly@redhat.com>
2022-12-01 13:48:53 -0500

    use assertions for sample data just because

--- day1/src/test/kotlin/com/antwerkz/aoc/day1/Day1Solution.kt
+++ day1/src/test/kotlin/com/antwerkz/aoc/day1/Day1Solution.kt
@@ -1,39 +1,36 @@
 package com.antwerkz.aoc.day1
 
 import java.io.File
+import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
 class Day1Solution {
     @Test
     fun part1() {
-        solvePart1("input1-sample.txt")
-        solvePart1("input1.txt")
+        assertEquals(solvePart1("input1-sample.txt"), 24000);
+        val liveData = solvePart1("input1.txt")
+
+        println("biggest load is ${liveData} calories")
     }
 
     @Test
     fun part2() {
         solvePart2("input1-sample.txt")
-        solvePart2("input1.txt")
+        assertEquals(solvePart2("input1-sample.txt"), 45000);
+        val liveData = solvePart2("input1.txt")
+
+        println("total of the biggest 3 is ${liveData} calories")
     }
 
-    private fun solvePart1(input: String) {
-        val elves = load(input)
+    private fun solvePart1(input: String) = load(input)
+        .map { elf -> elf to elf.load.sum() }
+        .maxBy { it.second }
+        .second
 
-        val biggest = elves.map { elf -> elf to elf.load.sum() }
-            .maxBy { it.second }
-
-        println("for ${input}: biggest is elf #${biggest.first.number} with ${biggest.second} calories")
-    }
-    private fun solvePart2(input: String) {
-        val elves = load(input)
-
-        val biggest = elves.map { elf -> elf to elf.load.sum() }
-            .sortedByDescending { it.second }
-            .take(3)
-
-        println("for ${input}: total of the biggest 3 is: " + biggest.sumOf {it.second})
-
-    }
+    private fun solvePart2(input: String) = load(input)
+        .map { elf -> elf to elf.load.sum() }
+        .sortedByDescending { it.second }
+        .take(3).sumOf { it.second }
 
     private fun load(input: String): MutableList<Elf> {
         val lines = File("src/test/resources/", input).readLines()
