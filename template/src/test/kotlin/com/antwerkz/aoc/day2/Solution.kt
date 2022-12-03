b919ba8eaca42def5958ccf219e798eb1da4f3b6
evanchooly <jlee@antwerkz.com>
2022-12-03 16:24:37 -0500

    update the template

--- template/src/test/kotlin/com/antwerkz/aoc/day2/Solution.kt
+++ template/src/test/kotlin/com/antwerkz/aoc/day2/Solution.kt
@@ -6,14 +6,14 @@ import org.testng.Assert.assertEquals
 class Solution : TestBase() {
     override fun day(): Int = somevalue
     override fun samplePart1() {
-        assertEquals(solvePart1(sample), somevalue)
+        assertEquals(solvePart1(sample), 0)
     }
 
     override fun samplePart2() {
-        assertEquals(solvePart2(sample), somevalue)
+        assertEquals(solvePart2(sample), 0)
     }
 
-    override fun solvePart1(input: List<String>) {}
+    override fun solvePart1(input: List<String>) = -1
 
-    override fun solvePart2(input: List<String>) {}
+    override fun solvePart2(input: List<String>) = -1
 }
