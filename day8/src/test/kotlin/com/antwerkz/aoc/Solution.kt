ec260ef6f268ac56f9694ab1db91c48b5ae351b4
evanchooly <jlee@antwerkz.com>
2022-12-09 00:23:27 -0500

    removed debug structures

--- day8/src/test/kotlin/com/antwerkz/aoc/Solution.kt
+++ day8/src/test/kotlin/com/antwerkz/aoc/Solution.kt
@@ -42,47 +42,15 @@ class Day8Solution : TestBase() {
     }
 
     private fun isVisible(forest: List<String>, row: Int, column: Int): Boolean {
-        val largest = object {
-            var up: Int = 0
-            var down: Int = 0
-            var left: Int = 0
-            var right: Int = 0
-
-            override fun toString() = "up:$up down:$down left:$left right:$right"
-        }
-        val visible = object {
-            var up = false
-            var down = false
-            var left = false
-            var right = false
-
-            override fun toString() = "up:$up down:$down left:$left right:$right"
-        }
         val target = forest[row][column].digitToInt()
 
-        largest.up = MaxStepper(forest, row, column, forest.size) { this.row-- }.run()
-        visible.up = largest.up < target
+        val up = MaxStepper(forest, row, column, forest.size) { this.row-- }.run() < target
+        val down  = MaxStepper(forest, row, column, forest.size) { this.row++ }.run() < target
+        val left = MaxStepper(forest, row, column, forest.size) { this.column-- }.run() < target
+        val right = MaxStepper(forest, row, column, forest.size) { this.column++ }.run() < target
 
-        largest.down = MaxStepper(forest, row, column, forest.size) { this.row++ }.run()
-        visible.down  = largest.down < target
-
-        largest.left = MaxStepper(forest, row, column, forest.size) { this.column-- }.run()
-        visible.left = largest.left < target
-
-        largest.right = MaxStepper(forest, row, column, forest.size) { this.column++ }.run()
-        visible.right = largest.right < target
-
-        return visible.up || visible.down || visible.left || visible.right
+        return up || down || left || right
     }
-
-    private fun visibleFromLeft(forest: List<String>, x: Int, y: Int) =
-        (0 until x).maxOf { forest[y][it] } < forest[y][x]
-    private fun visibleFromRight(forest: List<String>, x: Int, y: Int) =
-        ((x+1) until forest.size).maxOf { forest[y][it] } < forest[y][x]
-    private fun visibleFromUp(forest: List<String>, x: Int, y: Int) =
-        (0 until y).maxOf { forest[it][x] } < forest[y][x]
-    private fun visibleFromDown(forest: List<String>, x: Int, y: Int) =
-        ((y+1) until forest.size).maxOf { forest[it][x] } < forest[y][x]
 }
 
 open class Stepper(val forest: List<String>, var row: Int, var column: Int, var max: Int, val nextStep: Stepper.() -> Unit) {
