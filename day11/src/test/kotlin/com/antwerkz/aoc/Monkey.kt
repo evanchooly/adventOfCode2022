b0513840f9c088533117b161d5605468bf53a07f
Justin Lee <evanchooly@redhat.com>
2022-12-14 11:58:00 -0500

    part 2 sample works but not the live data

--- day11/src/test/kotlin/com/antwerkz/aoc/Monkey.kt
+++ day11/src/test/kotlin/com/antwerkz/aoc/Monkey.kt
@@ -1,14 +1,14 @@
 package com.antwerkz.aoc
 
 data class Monkey(
-    val items: MutableList<Int>,
-    val update: Int.() -> Int,
+    val items: MutableList<Long>,
+    val update: Long.() -> Long,
     val operation: Monkey.(List<Monkey>) -> Unit
 ) {
-    var inspected = 0
+    var inspected = 0L
     
-    fun inspect(monkies: List<Monkey>) {
-        this.operation(monkies)
+    fun inspect(monkeys: List<Monkey>) {
+        this.operation(monkeys)
     }
 
     override fun toString(): String {
--- day11/src/test/kotlin/com/antwerkz/aoc/Solution.kt
+++ day11/src/test/kotlin/com/antwerkz/aoc/Solution.kt
@@ -1,17 +1,28 @@
 package com.antwerkz.aoc
 
 class Day11Solution : TestBase() {
-    override fun day(): Int = 11
-    override fun sampleSolutionPart1() = 10605
+    override fun day() = 11
+    override fun sampleSolutionPart1() = 10605L
 
-    override fun sampleSolutionPart2() = TODO()
+    override fun sampleSolutionPart2() = 2713310158L
 
-    override fun solvePart1(input: List<String>): Int {
-        val monkeys = parse(input.iterator())
+    override fun solvePart1(input: List<String>): Long {
+        val monkeys = parse(input.iterator(), 3)
+        return watch(monkeys, 20)
+    }
 
-        repeat(20) {
+    override fun solvePart2(input: List<String>): Long {
+        val monkeys = parse(input.iterator(), 1)
+        return watch(monkeys, 10000)
+    }
+
+    private fun watch(monkeys: List<Monkey>, rounds: Int): Long {
+        (1..rounds).forEach {
             monkeys.forEach { monkey ->
                 monkey.inspect(monkeys)
+            }
+            if(it == 1 || it == 20 || it % 1000 == 0) {
+                println(monkeys.map { monkey -> monkey.inspected })
             }
         }
         return monkeys.sortedBy { it.inspected }
@@ -21,66 +32,75 @@ class Day11Solution : TestBase() {
             .first()
     }
 
-    private fun parse(input: Iterator<String>): List<Monkey> {
-        val monkies = mutableListOf<Monkey>()
+    private fun parse(input: Iterator<String>, worryAdjustment: Long): List<Monkey> {
+        val monkeys = mutableListOf<Monkey>()
         while (input.hasNext()) {
             val line = input.next()
             if (line.startsWith("Monkey ")) {
                 val worries = input.next()
                     .substringAfter(":")
                     .trim()
-                    .split(",")
-                    .map { it.trim().toInt() }
+                    .split(", ")
+                    .map { it.toLong() }
                     .toMutableList()
                 val operation = buildOperation(
                     input.next()
                         .substringAfter(" = ")
-                        .split(" ")
+                        .split(" "), worryAdjustment == 1L
                 )
-                val test = buildReaction(input)
+                val test = buildReaction(input, worryAdjustment)
                 
-                monkies += Monkey(worries, operation, test)
+                monkeys += Monkey(worries, operation, test)
             }
         }
         
-        return monkies
+        return monkeys
     }
 
-    private fun buildReaction(input: Iterator<String>): Monkey.(List<Monkey>) -> Unit {
-        var test = input.next().substringAfter(": ").trim()
-        if (!test.startsWith("divisible")) throw IllegalStateException("unexpected test: ${test}")
-        val divisor = test.substringAfterLast(" ").toInt()
+    private fun buildReaction(input: Iterator<String>, worryAdjustment: Long): Monkey.(List<Monkey>) -> Unit {
+        val divisor = input.next().substringAfterLast(" ").toLong()
         val trueMonkey = input.next().substringAfterLast(" ").toInt()
         val falseMonkey = input.next().substringAfterLast(" ").toInt()
 
-        return { monkies: List<Monkey> ->
+        return { monkeys: List<Monkey> ->
             while (items.isNotEmpty()) {
-                val newWorry = items.removeFirst().update() / 3
-                if (newWorry % divisor == 0) {
-                    monkies[trueMonkey].items += newWorry
+                val newWorry = items.removeFirst().update() / worryAdjustment
+                if (newWorry % divisor == 0L) {
+                    monkeys[trueMonkey].items += newWorry
                 } else {
-                    monkies[falseMonkey].items += newWorry
+                    monkeys[falseMonkey].items += newWorry
                 }
                 inspected++
             }
         }
     }
 
-    private fun buildOperation(equation: List<String>): (Int) -> Int {
+    private fun buildOperation(equation: List<String>, truncate: Boolean = false): (Long) -> Long {
+        val MODULO = 23*19*13*17
         return when (equation[1]) {
             "*" -> {
-                if (equation[2] != "old") { old -> old * equation[2].toInt() }
-                else { old -> old * old }
+                if (equation[2] != "old") { old -> var new = old * equation[2].toLong()
+                    if(truncate) new %= MODULO
+                    new
+                }
+                else { old -> var new = old * old
+                    if(truncate) new %= MODULO
+                    new
+                }
             }
 
             "+" -> {
-                if (equation[2] != "old") { old -> old + equation[2].toInt() }
-                else { old -> old + old }
+                if (equation[2] != "old") { old -> var new = old + equation[2].toLong()
+                    if(truncate) new %= MODULO
+                    new
+                }
+                else { old -> var new = old + old
+                    if(truncate) new %= MODULO
+                    new
+                }
             }
 
             else -> throw IllegalStateException("unknown operation: ${equation[1]}")
         }
     }
-
-    override fun solvePart2(input: List<String>) = TODO()
 }
